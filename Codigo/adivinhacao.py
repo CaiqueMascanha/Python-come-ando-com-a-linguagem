9c4cf49dea0a6e43a86c4af91f3724fef130d208
Caique Mascanha <caiquesilva820@gmail.com>
2023-01-19 18:58:41 -0300

    Aprendendo sobre FOR

--- Codigo/adivinhacao.py
+++ Codigo/adivinhacao.py
@@ -9,13 +9,17 @@ total_de_tentativas = 3
 # O "WHILE" TORNA UMA REPETIÇÃO
 for rodada in range(1, total_de_tentativas + 1):
   print(f"Tentativa {rodada} de: {total_de_tentativas}")
-  chute = int(input("Digite um número: "))
+  chute = int(input("Digite um número entre 1 e 100: "))
   acertou = chute == numero_secreto
   maior = chute > numero_secreto
 
-
+  if(chute < 1 or chute > 100):
+    print("Você deve digitar um número entre 1 e 100!")
+    continue #COM ESTE COMENDO NÃO ENCERRA O LAÇO
+  
   if(acertou):
     print("Você acertou!")
+    break #COM ISSO ENCERRAMOS O LAÇO
   elif(maior):
     print("Você errou! Seu chute foi acima")
   else:
