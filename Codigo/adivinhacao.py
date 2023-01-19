8fbbf5597095b5217a44d729a3c5bf4d4b241eaa
Caique Mascanha <caiquesilva820@gmail.com>
2023-01-19 16:36:27 -0300

    Aprendendo sobre While

--- Codigo/adivinhacao.py
+++ Codigo/adivinhacao.py
@@ -4,18 +4,23 @@ print("********************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-chute = int(input("Digite um número: "))
+rodada = 1
+
+# O "WHILE" TORNA UMA REPETIÇÃO
+while(rodada <= total_de_tentativas):
+  print(f"Tentativa {rodada} de:", total_de_tentativas)
+  chute = int(input("Digite um número: "))
+  acertou = chute == numero_secreto
+  maior = chute > numero_secreto
 
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-
-
-if(acertou):
-  print("Você acertou!")
-elif(maior):
-  print("Você errou! Seu chute foi acima")
-else:
-  print("Você errou! Seu chute foi abaixo")
+  if(acertou):
+    print("Você acertou!")
+  elif(maior):
+    print("Você errou! Seu chute foi acima")
+  else:
+    print("Você errou! Seu chute foi abaixo")
+    
+  rodada = rodada + 1
   
 print("Fim do jogo!")
