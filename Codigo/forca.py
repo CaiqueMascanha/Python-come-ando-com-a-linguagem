ee33739396cb40afa3af3bb05ed376d92fe455df
Caique Mascanha <caiquesilva820@gmail.com>
2023-01-20 09:45:26 -0300

    Forca

--- Codigo/forca.py
+++ Codigo/forca.py
@@ -2,6 +2,25 @@ def jogar():
     print("***************************")
     print("Bem vindo no jogo de Forca!")
     print("***************************")
+    
+    palavra_secreta = "banana"
+    print("A palavra secreta possui :"len(palavra_secreta))
+    
+    enforcou = False
+    acertou = False
+    
+    #ENQUANDO NÃO ENFORCOU E NÃO ACERTOU
+    while(not enforcou and not acertou):
+      
+      chute = input("Qual letra?")
+      
+      index = 0
+      for letra in palavra_secreta:
+        if(chute == letra):
+          print(f"encontrei a letra {letra} na posição {index}")
+        index = index + 1
+      
+      print("Jogando...")
       
     print("Fim do jogo!")
   
