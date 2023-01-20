0cabe2aa022a2b6887e8043fe7ece4f7af4015a6
Caique Mascanha <caiquesilva820@gmail.com>
2023-01-19 22:08:53 -0300

    Finalizando curso

--- Codigo/adivinhacao.py
+++ Codigo/adivinhacao.py
@@ -1,32 +1,48 @@
 import random
 
-print("********************************")
-print("Bem vindo no jogo de Adivinhação")
-print("********************************")
+def jogar():
+  print("********************************")
+  print("Bem vindo no jogo de Adivinhação")
+  print("********************************")
 
-numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
-print(numero_secreto)
+  numero_secreto = random.randrange(1,101)
+  total_de_tentativas = 0
+  pontos = 1000
+  print("Qual o níel de dificuldade?")
+  print("(1) Fácil (2) Médio (3) Difícil")
 
-# O "WHILE" TORNA UMA REPETIÇÃO
-for rodada in range(1, total_de_tentativas + 1):
-  print(f"Tentativa {rodada} de: {total_de_tentativas}")
-  chute = int(input("Digite um número entre 1 e 100: "))
-  acertou = chute == numero_secreto
-  maior = chute > numero_secreto
-
-  if(chute < 1 or chute > 100):
-    print("Você deve digitar um número entre 1 e 100!")
-    continue #COM ESTE COMENDO NÃO ENCERRA O LAÇO
-  
-  if(acertou):
-    print("Você acertou!")
-    break #COM ISSO ENCERRAMOS O LAÇO
-  elif(maior):
-    print("Você errou! Seu chute foi acima")
+  nivel = int(input("Define o nível: "))
+  if(nivel == 1):
+    total_de_tentativas = 20
+  elif(nivel == 2):
+    total_de_tentativas = 10
   else:
-    print("Você errou! Seu chute foi abaixo")
+    total_de_tentativas = 5
     
+  # O "WHILE" TORNA UMA REPETIÇÃO
+  for rodada in range(1, total_de_tentativas + 1):
+    print(f"Tentativa {rodada} de: {total_de_tentativas}")
+    chute = int(input("Digite um número entre 1 e 100: "))
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+
+    if(chute < 1 or chute > 100):
+      print("Você deve digitar um número entre 1 e 100!")
+      continue #COM ESTE COMENDO NÃO ENCERRA O LAÇO
+    
+    if(acertou):
+      print(f"Você acertou e fez {pontos} pontos!")
+      break #COM ISSO ENCERRAMOS O LAÇO
+    elif(maior):
+      print("Você errou! Seu chute foi acima")
+    else:
+      print("Você errou! Seu chute foi abaixo")
+    pontos_perdidos = abs(numero_secreto - chute)
+    pontos = pontos - pontos_perdidos
+      
+    
+    
+  print("Fim do jogo!")
   
-  
-print("Fim do jogo!")
+if(__name__ == "__main__"):
+  jogar()
